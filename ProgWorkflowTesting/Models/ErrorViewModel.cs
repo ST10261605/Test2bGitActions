e20e1d59d57193ce18d86cb9a42a54ee554fa224
ST10261605 <ST10261605@vcconnect.edu.za>
2024-10-11 08:29:55 +0200

    Add project files.

---
+++ ProgWorkflowTesting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgWorkflowTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
